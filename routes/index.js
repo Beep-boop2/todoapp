197e86b0cd3cf48654460cbbb29827dffae6f056
beep-boop2 <ijio@fresnoU.org>
2025-01-29 09:01:15 -0800

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const toDoController = require("../controllers/todo_controller.js")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+router.get('/', toDoController.homeRoute);
 
+router.get('/add', toDoController.renderAddForm);
+
+router.post('/add', toDoController.addNewItems);
+
+router.get('/complete/:id', toDoController.markItemAsComplete);
+
+router.get('/incomplete/:id', toDoController.markItemAsIncomplete);
+
+router.get('/delete/:id', toDoController.deleteItem);
+
+router.get('/edit/:id', toDoController.renderEditForm)
+
+router.post('/edit/:id', toDoController.updateItem)
 module.exports = router;
